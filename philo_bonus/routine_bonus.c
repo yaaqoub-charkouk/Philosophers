adb7a66ca2a9a7d169c74937d16008246ffd0624
yaaqoub <yaaqoubcourses@gmail.com>
2025-04-01 16:22:27 +0000

    finished bonus

--- philo_bonus/routine_bonus.c
+++ philo_bonus/routine_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ycharkou <ycharkou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 12:00:37 by ycharkou          #+#    #+#             */
-/*   Updated: 2025/03/30 16:24:01 by ycharkou         ###   ########.fr       */
+/*   Updated: 2025/04/01 16:21:38 by ycharkou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,24 +39,20 @@ void	*death_monitoring(void	*parameter)
 		sem_wait(philo->data->death_sem);
 		last_meal = philo->last_meal;
 		num_eaten = philo->num_eaten;
-		sem_post(philo->data->death_sem);
-		// to avoi data race ;
-		
+		sem_post(philo->data->death_sem);		
 		if (get_current_time(philo->data) - last_meal >= philo->data->time_to_die)
 		{
 			sem_wait(philo->data->print);
 			printf("\033[1;31m%zu %zu died\033[0m\n",
 				get_current_time(philo->data), philo->id);
-			// sem_post(philo->data->is_died);
 			exit(1);
 		}
 		if (philo->data->max_eating_count_p != -2 && num_eaten == philo->data->max_eating_count_p)
 		{
-			usleep(philo->data->time_to_eat);
-			// printf("oussama\n");
-			// sem_post(philo->data->is_finished);
-			if (philo->data->max_eating_count_p != 1)			
-				exit(0);
+			// usleep(philo->data->time_to_eat);
+			// printf("%zu\n", philo->id);
+			if (philo->id == philo->data->num_of_philos)			
+				exit(1);
 		}
 		usleep(500);
 	}
@@ -66,16 +62,10 @@ void	*death_monitoring(void	*parameter)
 
 void	routine(t_data *data, t_philo *philo)
 {
-	// time_t	last_meal;
 	pthread_t	death_monitor;
 
-	pthread_create(&death_monitor, NULL, death_monitoring, philo); // handle failure !!!!!!!!!
+	pthread_create(&death_monitor, NULL, death_monitoring, philo);
 	pthread_detach(death_monitor);
-	// there is a problem 
-	// sem_unlink("/death_sem");
-	// philo->death_sem = sem_open("/death_sem", O_CREAT | O_EXCL, 0644, 1);
-	
-	// last_meal = get_current_time(data);
 	if (data->num_of_philos == 1)
 	{
 		write_log(data, philo->id, "has taken a fork");
@@ -86,32 +76,30 @@ void	routine(t_data *data, t_philo *philo)
 	}
 	while (1)
 	{
-		take_forks(data, philo);
-		write_log(data, philo->id, "is eating");
-		
-		// avoid data race 
-		sem_wait(data->death_sem);
-		philo->last_meal = get_current_time(data);
-		sem_post(data->death_sem);
-		
-		sem_wait(data->death_sem);
-		philo->last_meal = get_current_time(data);
-		sem_post(data->death_sem);
-		
-		usleep(data->time_to_eat * 1000); // time to eat
-		
-		sem_wait(data->death_sem);
-		philo->num_eaten++;
-		sem_post(data->death_sem);
-		
-		// avoid data race 
-		
-		sem_post(data->forks);
-		sem_post(data->forks);
-		write_log(data, philo->id, "is sleeping");
-		usleep(data->time_to_sleep * 1000); // time to sleep 
-		write_log(data, philo->id, "is thinking");
-		
+		if (philo->num_eaten != philo->data->max_eating_count_p)
+		{
+			write_log(data, philo->id, "is thinking");
+			take_forks(data, philo);
+			write_log(data, philo->id, "is eating");
+			
+			// avoid data race 
+			sem_wait(data->death_sem);
+			philo->last_meal = get_current_time(data);
+			sem_post(data->death_sem);
+			
+			usleep(data->time_to_eat * 1000); // time to eat
+			
+			sem_wait(data->death_sem);
+			philo->num_eaten++;
+			sem_post(data->death_sem);
+					
+			sem_post(data->forks);
+			sem_post(data->forks);
+			
+			write_log(data, philo->id, "is sleeping");
+			usleep(data->time_to_sleep * 1000); // time to sleep 
+			write_log(data, philo->id, "is thinking");
+		}
 	}
 	exit(0);
 }
